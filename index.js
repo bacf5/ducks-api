2a8d877f7f2f2c34dd46da07e161354773eb3026
bacf <brunoagustincarusofassa@gmail.com>
2025-08-22 01:07:40 +0200

    Added express.static for path public

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api', factsRoutes);
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,7 +21,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('sup');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
